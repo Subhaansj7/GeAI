1e402012e4fc7cd7c559c8e0ef5b7fe3fffde98c
Subhaan Jirait <101044609+Subhaansj7@users.noreply.github.com>
2025-11-29 12:10:16 +0530

    Add files via upload

---
+++ Langchain/LCEL chain.py
@@ -0,0 +1,21 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+chat=ChatGoogleGenerativeAI(
+    model='gemini-2.5-flash',
+    google_api_key='AIzaSyC2ULnL0oBfAfjEGGj6hl52aZ-oe2OcaMI'
+)
+
+output_parser= StrOutputParser()
+
+prompt=ChatPromptTemplate.from_template(
+    'You are a helpful assistant. Provide a simple, 2-point summary of the concept: {concept}'
+
+)
+
+chain= prompt | chat | output_parser
+
+response=chain.invoke({'concept':'Recursion in programming'})
+
+print(response)
